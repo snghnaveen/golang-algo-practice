838e0b4d40dac152f5a3bcb56fccfb06f098e9d0
Naveen Singh <naveen@naveenss320.local>
2023-03-27 23:13:58 +0530

    add test string test case

--- string/README.md
+++ string/README.md
@@ -4,6 +4,9 @@ Content :
 ### Index
 | Question       | Source |
 | -------- |   ------------- |
-| 1. Check if given strings are rotations of each other or not   | [string_rotation_test.go](./string_rotation_test.go)  |
+| 1. Check if given strings are rotations of each other or not   | [rotation_test.go](./rotation_test.go)  |
 | 2. Given a set of strings, find the longest common prefix   | [longest-common_prefix_using_sorting_test.go](./longest-common_prefix_using_sorting_test.go)  |
+| 2. Check whether two given strings are Permutation of each other   | [permutation_test.go](./permutation_test.go)  |
+
+
 
---
+++ string/permutation_test.go
@@ -0,0 +1,51 @@
+package string
+
+import (
+	"sort"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestArePermutation(t *testing.T) {
+	t.Log(`Write a function to check whether two given strings are Permutation of each other or not. A Permutation of a string is another string that contains same characters, only the order of characters can be different. For example, “abcd” and “dabc” are Permutation of each other.`)
+
+	t.Run("When string are in Permutation", func(t *testing.T) {
+
+		str1 := "abcd"
+		str2 := "dabc"
+		assert.Equal(t, true, RunTestArePermutation(str1, str2))
+	})
+
+	t.Run("When string are not in Permutation", func(t *testing.T) {
+		str1 := "abcd"
+		str2 := "qwer"
+		assert.Equal(t, false, RunTestArePermutation(str1, str2))
+	})
+
+}
+
+func RunTestArePermutation(str1, str2 string) bool {
+
+	str1Arr := strings.Split(str1, "")
+	str2Arr := strings.Split(str2, "")
+
+	sort.Strings(str1Arr)
+	sort.Strings(str2Arr)
+
+	n1 := len(str1Arr)
+	n2 := len(str2Arr)
+
+	if n2 != n1 {
+		return false
+	}
+
+	for i := 0; i < n1; i++ {
+		if str1Arr[i] != str2Arr[i] {
+			return false
+		}
+	}
+
+	return true
+}

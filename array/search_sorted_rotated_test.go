97f58f173b11c7ef938e3190d0156c6aa0262573
Naveen Singh <sngh.naveen@gmail.com>
2024-10-18 19:32:21 +0530

    Fix for few case

--- array/search_sorted_rotated_test.go
+++ array/search_sorted_rotated_test.go
@@ -29,43 +29,44 @@ func TestSearchSortedRotated(t *testing.T) {
 	`)
 
 	t.Run("Suite 1", func(t *testing.T) {
-		in := []int{4, 5, 6, 7, 0, 1, 2}
+		nums := []int{4, 5, 6, 7, 0, 1, 2}
 		target := 0
 		exp := 4
-		assert.Equal(t, exp, RunSearchSortedRotated(target, in))
+		assert.Equal(t, exp, RunSearchSortedRotated(nums, target))
 	})
 
 	t.Run("Suite 2", func(t *testing.T) {
-		in := []int{4, 5, 6, 7, 0, 1, 2}
+		nums := []int{4, 5, 6, 7, 0, 1, 2}
 		target := 3
 		exp := -1
-		assert.Equal(t, exp, RunSearchSortedRotated(target, in))
+		assert.Equal(t, exp, RunSearchSortedRotated(nums, target))
 	})
 }
 
-func RunSearchSortedRotated(target int, in []int) int {
-	left, right := 0, len(in)-1
+func RunSearchSortedRotated(nums []int, target int) int {
+	l := 0
+	r := len(nums) - 1
 
-	for left <= right {
-		mid := left + (right-left)/2
-
-		if in[mid] == target {
+	for l <= r {
+		mid := l + (r-l)/2
+		if nums[mid] == target {
 			return mid
 		}
 
-		if in[left] < in[mid] {
-			if in[left] <= target && target < in[mid] {
-				right = mid - 1
+		if nums[l] <= nums[mid] {
+			if nums[l] <= target && target < nums[mid] {
+				r = mid - 1
 			} else {
-				left = mid + 1
+				l = mid + 1
 			}
 		} else {
-			if in[mid] < target && target <= in[right] {
-				left = mid + 1
+			if nums[mid] < target && target <= nums[r] {
+				l = mid + 1
 			} else {
-				right = mid - 1
+				r = mid - 1
 			}
 		}
 	}
+
 	return -1
 }
